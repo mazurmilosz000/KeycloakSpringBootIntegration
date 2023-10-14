ceb8e0c1214be036acc09a1f8ead741a4ebcda3f
Milosz <mazur.milosz000@gmail.com>
2023-10-14 14:13:04 +0200

    feat: added JwtAuthConverter implementation

--- keycloak-integration/src/main/java/com/mazurmilosz/keycloakintegration/configuration/JwtAuthConverter.java
+++ keycloak-integration/src/main/java/com/mazurmilosz/keycloakintegration/configuration/JwtAuthConverter.java
@@ -1,35 +1,69 @@
 package com.mazurmilosz.keycloakintegration.configuration;
 
 import lombok.RequiredArgsConstructor;
-
-
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
+   private final KeycloakProperties keycloakProperties;
     private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
 
     @Override
     public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
-        var authorities = Stream.concat(
+        Collection<GrantedAuthority> authorities = Stream.concat(
                 jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
-                extractResourceRoles(jwt).stream() // fixme
+                extractResourceRoles(jwt).stream()
+        )
+                .collect(Collectors.toSet());
+
+        return new JwtAuthenticationToken(
+                jwt,
+                authorities,
+                getPrincipalClaimName(jwt)
         );
-        return null;
+    }
+
+    private String getPrincipalClaimName(Jwt jwt) {
+        String claimName = JwtClaimNames.SUB;
+        if (keycloakProperties.getPrincipalAttribute() != null) {
+            claimName = keycloakProperties.getPrincipalAttribute();
+        }
+
+        return jwt.getClaim(claimName);
     }
 
     private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
-        // TODO: need to be implemented!
-        return null;
+        Map<String, Object> resourceAccess;
+        Map<String, Object> resources;
+        Collection<String> resourceRoles;
+        if (jwt.getClaim("resource_access") == null) {
+            return Set.of();
+        }
+        resourceAccess = jwt.getClaim("resource_access");
+        if (resourceAccess.get(keycloakProperties.getResourceAccessId()) == null) {
+            return Set.of();
+        }
+        resources = (Map<String, Object>) resourceAccess.get(keycloakProperties.getResourceAccessId());
+        resourceRoles = (Collection<String>) resources.get("roles");
+
+        return resourceRoles.stream()
+                .map(role -> new SimpleGrantedAuthority(String.format("ROLE_%s", role))).collect(Collectors.toSet());
     }
 }
